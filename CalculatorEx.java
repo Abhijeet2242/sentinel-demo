d0fd01da567f4799a0f1d1a9fa3db86ecd7679a5
Abhijeet Dhopare <126075104+Abhijeet2242@users.noreply.github.com>
2026-07-11 22:25:49 +0530

    Implement basic subtraction in CalculatorEx

---
+++ CalculatorEx.java
@@ -0,0 +1,11 @@
+public class CalculatorEx {
+
+    public static int Arithmetic(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        int result = Arithmetic(20, 8);
+        System.out.println("Subtraction Result: " + result);
+    }
+}
